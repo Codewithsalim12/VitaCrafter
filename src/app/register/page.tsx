70805cc1b66db8d304d5079f1c3a31b414b7e5a7
Mohammad salim <mohammadsalimmir13@gmail.com>
2025-06-25 08:17:28 +0530

    fixed the latest errors

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -21,7 +21,7 @@ import {
   FormLabel,
 } from "@/components/ui/form";
 import { useToast } from "@/hooks/use-toast";
-import { useSession, signIn } from "next/auth/react";
+import { useSession, signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import Link from "next/link";
